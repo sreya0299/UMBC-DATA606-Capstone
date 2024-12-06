a6bc3113485a6487d30074c813225cc525fd79c1
sreya0299 <57681407+sreya0299@users.noreply.github.com>
2024-12-05 23:26:07 -0500

    Update test.py.py

--- app/test.py.py
+++ app/test.py.py
@@ -25,12 +25,7 @@ nltk.download('averaged_perceptron_tagger_eng')
 
 # Load Models and Preprocessing Tools
 model_files = {
-    "Logistic Regression": "app/Logistic_Regression.pkl",
-    "SVM (Linear)": "app/SVM_Linear.pkl",
-    "SVM (RBF)": "app/SVM_RBF.pkl",
-    "Naive Bayes": "app/Naive_Bayes.pkl",
-    "Random Forest": "app/Random_Forest.pkl",
-    "Decision Tree": "app/Decision_Tree.pkl"
+    "SVM (RBF)": "app/SVM_RBF.pkl"
 }
 
 models = {}
