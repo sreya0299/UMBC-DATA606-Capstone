0505b8587387c381d474c0386c0d2b2d8a750adb
sreya0299 <57681407+sreya0299@users.noreply.github.com>
2024-12-05 23:56:26 -0500

    Update test.py.py

--- app/test.py.py
+++ app/test.py.py
@@ -19,7 +19,7 @@ import time  # For spinners
 import plotly.express as px  # For visualizations
 
 # NLTK setup
-nltk.download('punkt-tab')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger_eng')
 
